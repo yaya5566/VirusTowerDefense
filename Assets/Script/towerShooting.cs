86fc74019ac4f4c006cdfcba474aa0273e69838b
tsai4000 <tsai4000nise+1@gmail.com>
2020-02-01 17:38:06 +0800

    Add tower shooting dectecting

---
+++ Assets/Script/towerShooting.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class towerShooting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public int Range;
+    public string TowerName;
+    public GameObject enemyGenerator;
+
+    private GameObject CurrentTarget;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CurrentTarget == null) {
+            SearchTarget();
+        }
+        else if(CurrentTarget != null) {
+            Vector3 targetPosition = new Vector3(CurrentTarget.transform.position.x, transform.position.y, transform.position.z);
+            if (Vector3.Distance(transform.position, targetPosition) > Range || CurrentTarget.tag == "Repaired") {
+                CurrentTarget = null;
+                return;
+            }
+        }
+    }
+
+    void SearchTarget() 
+    {
+        foreach (var enemy in enemyGenerator.GetComponent<enemyGenerator>().Enemies){
+            Vector3 target = new Vector3(enemy.transform.position.x, enemy.transform.position.y, 0);
+            if (enemy.tag != gameObject.tag) {
+                continue;
+            }
+            if (Vector3.Distance(transform.position, target) <= Range) {
+                CurrentTarget = enemy;
+            }
+        }
+    }
+}
