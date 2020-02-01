726c82d807fd52f0499e9b8f036a0f0ed3c2cec9
tsai4000 <tsai4000nise+1@gmail.com>
2020-02-01 19:52:51 +0800

    Add tower shooting bullet script

--- Assets/Script/towerShooting.cs
+++ Assets/Script/towerShooting.cs
@@ -11,6 +11,8 @@ public class towerShooting : MonoBehaviour
     public enemyGenerator eg;
 
     private GameObject CurrentTarget;
+    public GameObject bulletObj;
+    private bool CD = true;
     void Start()
     {
         eg = FindObjectOfType<enemyGenerator>();
@@ -23,15 +25,29 @@ public class towerShooting : MonoBehaviour
             SearchTarget();
         }
         else if(CurrentTarget != null) {
-            Debug.Log(CurrentTarget.name);
             Vector3 targetPosition = new Vector3(CurrentTarget.transform.position.x, transform.position.y, transform.position.z);
             if (Vector3.Distance(transform.position, targetPosition) > Range || CurrentTarget.tag == "Repaired") {
                 CurrentTarget = null;
                 return;
+            }else{
+                Shooting();
             }
         }
     }
 
+    void RunCD()
+    {
+        CD = true;
+    }
+    void Shooting()
+    {
+        if(CD == true) {
+            GameObject bullet = UnityEngine.Object.Instantiate<GameObject>(bulletObj, transform);
+            bullet.GetComponent<bulletItem>().target = CurrentTarget;
+            CD = false;
+            Invoke("RunCD", 0.7f);
+        }
+    }
     void SearchTarget() 
     {
         foreach (var enemy in eg.Enemies){
