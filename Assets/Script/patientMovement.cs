98e87be5e86ee035dc30ee789f4399b414c8cba3
WoodCat2018 <41362610+WoodCat2018@users.noreply.github.com>
2020-02-01 15:09:51 +0800

    更新場景(TileMap)+病患移動腳本修改
    1. 新增TileMap用場景（2層）
    2. 新增醫院場景的TilePalette
    3. 新增場景用圖片資源
    4. 新增病患移動腳本
    5. 修改病患參數值腳本(移除移動邏輯)

---
+++ Assets/Script/patientMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // movement.x = Input.GetAxisRaw("Horizontal");
+        // movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate(){
+        movement.x = 0.1f;
+        rb.MovePosition(rb.position + movement * moveSpeed *Time.fixedDeltaTime);
+    }
+}
