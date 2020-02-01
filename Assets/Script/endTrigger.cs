8f9864dfb494cf61ef97585fb6993769e7185621
WoodCat2018 <41362610+WoodCat2018@users.noreply.github.com>
2020-02-01 19:26:14 +0800

    修改地圖切圖、新增終點觸發
    1. 修正地圖切圖大小錯誤
    2. 新增終點觸發圖層
    3. 更換病人衣服圖像

---
+++ Assets/Script/endTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endTrigger : MonoBehaviour
+{
+
+    void Start(){
+        Debug.Log("start");
+    }
+
+
+    void OnTriggerEnter2D(Collider2D Coll){
+        Debug.Log("OnTrigger"+Coll.gameObject.ToString());
+        Destroy(Coll.gameObject);
+    }
+
+}
