f3a94d57421820f502e225b2dbfe9c70e2e0411c
yaya5566 <william855338@gmail.com>
2020-02-02 12:22:05 +0800

    開始場景

---
+++ Assets/Script/startScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class startScene : MonoBehaviour
+{
+    [SerializeField]
+    private Button btnStart; 
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        btnStart.onClick.AddListener(delegate() {
+            SceneManager.LoadScene("HospitalScene");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
