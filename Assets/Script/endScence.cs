fd193c60e42a9e6634efc56f84358cc992c1a1f3
yaya5566 <william855338@gmail.com>
2020-02-02 16:28:43 +0800

    結算

--- Assets/Script/endScence.cs
+++ Assets/Script/endScence.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class endScence : MonoBehaviour
 {
     [SerializeField]
     private Button btnEnd;
+    [SerializeField]
+    private Button btnRe;
     // Start is called before the first frame update
     void Start()
     {
         btnEnd.onClick.AddListener(delegate() {
             Application.Quit();
+        });
+        btnRe.onClick.AddListener(delegate() {
+            SceneManager.LoadScene("StartScene");
         });
     }
 
